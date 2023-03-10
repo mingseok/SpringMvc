9f0481fe47776ea02415e5e7c5806c7f9960cc7a
minseok <alstjr706@naver.com>
2023-03-10 21:36:26 +0900

    SpringMVC : mvc 프레임워크 만들기 v2 (디스패처)

---
+++ src/main/java/com/example/springmvc/web/frontcontroller/v2/controller/MemberListControllerV2.java
@@ -0,0 +1,28 @@
+package com.example.springmvc.web.frontcontroller.v2.controller;
+
+import com.example.springmvc.domain.Member;
+import com.example.springmvc.domain.MemberRepository;
+import com.example.springmvc.web.frontcontroller.MyView;
+import com.example.springmvc.web.frontcontroller.v2.ControllerV2;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+public class MemberListControllerV2 implements ControllerV2 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public MyView process(HttpServletRequest request,
+                          HttpServletResponse response) throws ServletException, IOException {
+
+        List<Member> members = memberRepository.findAll();
+        request.setAttribute("members", members);
+
+        return new MyView("/WEB-INF/views/members.jsp");
+    }
+}
